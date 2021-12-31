f1006a00a32fc8a638022fe55f4533ce17f1956d
Maniteja <78750315+katammaniteja@users.noreply.github.com>
2021-12-31 23:08:10 +0530

    Create Subset Sum Problem.cpp

---
+++ dp/Subset Sum Problem.cpp
@@ -0,0 +1,15 @@
+class Solution{   
+public:
+    bool isSubsetSum(int n, int arr[], int sum){
+        bool dp[n+1][sum+1];
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        for(int i=1;i<=sum;i++) dp[0][i]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(arr[i-1]>j) dp[i][j]=dp[i-1][j];
+                else dp[i][j]=dp[i-1][j] || dp[i-1][j-arr[i-1]];
+            }
+        }
+        return dp[n][sum];
+    }
+};

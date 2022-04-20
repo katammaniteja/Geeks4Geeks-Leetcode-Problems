0dea781c60d7536711e0a24a68d659a499e91d7d
unknown <katammaniteja@gmail.com>
2022-04-20 10:55:35 +0530

    heap sort updated

--- heaps/HeapSort.cpp
+++ heaps/HeapSort.cpp
@@ -1,40 +1,41 @@
-class Solution
-{
-    public:
-    //Heapify function to maintain heap property.
-    void heapify(int arr[], int n, int i)  
-    {
-      int largest=i,left=2*i+1,right=2*i+2;
-      if(left<n && arr[left]>arr[largest]){
-          largest=left;
-      }
-      if(right<n && arr[right]>arr[largest]){
-          largest=right;
-      }
-      if(largest!=i){
-          swap(arr[largest],arr[i]);
-          heapify(arr,n,largest);
-      }
-    }
-
-    public:
-    //Function to build a Heap from array.
-    void buildHeap(int arr[], int n)  
-    {
-        for(int i=(n/2)-1;i>=0;i--){
-            heapify(arr,n,i);
-        }
-    }
-
-    
-    public:
-    //Function to sort an array using Heap Sort.
-    void heapSort(int arr[], int n)
-    {
-        buildHeap(arr,n);
-        for(int i=n-1;i>=1;i--){
-            swap(arr[0],arr[i]);
-            heapify(arr,i,0);
-        }
-    }
-};
+#include<iostream>
+using namespace std;
+void heapify(int arr[],int n,int i){
+	int largest=i,left=2*i+1,right=2*i+2;
+	if(left<n && arr[left]>arr[largest]) largest=left;
+	if(right<n && arr[right]>arr[largest]) largest=right;
+	if(largest!=i){
+		swap(arr[largest],arr[i]);
+		heapify(arr,n,largest);
+	}
+}
+void buildHeap(int arr[],int n){
+	for(int i=(n/2)-1;i>=0;i--){
+		heapify(arr,n,i);
+	}
+}
+void heap_sort(int arr[],int n){
+	buildHeap(arr,n);
+	for(int i=n-1;i>=1;i--){
+		swap(arr[0],arr[i]);
+		heapify(arr,i,0);
+	}
+}
+void print(int arr[],int n){
+	for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+	cout<<endl;
+}
+int main(){
+	int n;
+	cout<<"Enter the number of elements: ";
+	cin>>n;
+	int arr[n];
+	cout<<"Enter the elements: ";
+	for(int i=0;i<n;i++) cin>>arr[i];
+	cout<<"The elements before sorting are: ";
+	print(arr,n);
+	heap_sort(arr,n);
+	cout<<"The elements after sorting are: ";
+	print(arr,n);
+	return 0;
+}
